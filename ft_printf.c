f8dda6d6deaabf8dddb74a6019d90b32897d214e
costaneto <dzneto50@gmail.com>
2022-11-21 20:50:30 +0000

    Planning function

--- ft_printf.c
+++ ft_printf.c
@@ -6,28 +6,32 @@
 /*   By: dneto <dneto@student.42lisboa.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 19:51:05 by dneto             #+#    #+#             */
-/*   Updated: 2022/11/21 20:18:23 by dneto            ###   ########.fr       */
+/*   Updated: 2022/11/21 20:49:45 by dneto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 
-
-char	*ft()
+int	ft_getcount(char *str)
 {
+	int	i;
 
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
-
 
 int	ft_printf(const char *str, ...)
 {
 	int		count;
 	va_list	args;
 
+	count = ft_getcount(str);
+
 	/*
 	count number of args with % -> if c == % && c + 1 == (one of conversions)
 										count ++;
 	*/
-
 	count = ft();
 }
