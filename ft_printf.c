9f4b40f738624067134f165eed3d2f5c343650f2
costaneto <dzneto50@gmail.com>
2022-11-23 19:57:19 +0000

    Adding auxiliar functions

--- ft_printf.c
+++ ft_printf.c
@@ -6,23 +6,75 @@
 /*   By: dneto <dneto@student.42lisboa.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 19:51:05 by dneto             #+#    #+#             */
-/*   Updated: 2022/11/23 17:04:08 by dneto            ###   ########.fr       */
+/*   Updated: 2022/11/23 19:54:52 by dneto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
+#include <unistd.h>
 
-int	ft_printf(const char *str, ...)
+int	ft_putchar(char c, int count)
 {
-	int		count;
-	va_list	args;
+	write(1, &c, 1);
+	return (++count);
+}
 
-	va_start(args, str);
-
+int	ft_putstr(char *s, int count)
+{
 
 }
 
-/*
-	count number of args with % -> if c == % && c + 1 == (one of conversions)
-										count ++;
-*/
+int	ft_putnbr(int n, int count)
+{
+
+}
+
+int	print_handler(char *s, va_list ap)
+{
+	int	count;
+
+	count = 0;
+	while (*s)
+	{
+		if (*s != '%')
+		{
+			count = ft_putchar(*s++, count);
+			continue ;
+		}
+		if (*++s == '%')
+			count = ft_putchar('%', count);
+		else if (*++s == 'c')
+			count = ft_putchar(va_arg(ap, char), count);
+		else if (*++s == 's')
+			count = ft_putchar(va_arg(ap, char *), count);
+		else if (*++s == 'p')
+			count = ft_putchar(va_arg(ap, void *), count);
+		else if (*++s == 'd')
+			count = ft_putchar(va_arg(ap, int), count);
+		else if (*++s == 'i')
+			count = ft_putchar(va_arg(ap, int), count);
+		else if (*++s == 'u')
+			count = ft_putchar(va_arg(ap, unsigned int), count);
+		else if (*++s == 'x')
+			count = ft_putchar(va_arg(ap, int), count);
+		else if (*++s == 'X')
+			count = ft_putchar(va_arg(ap, int), count);
+	}
+}
+
+cspdiuxX%
+
+int	ft_printf(const char *s, ...)
+{
+	int		count;
+	va_list	ap;
+
+	if (!s)
+		return (NULL);
+	if (!*s)
+		return (0);
+	va_start(ap, s);
+	count = print_handler(s, ap);
+	va_end(ap);
+	return (count);
+}
